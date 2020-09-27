788e1c65a9294e62257f8aa6ec7b9591275c6ff4
Ameco <hamecow@gmail.com>
2020-09-26 20:50:04 -0700

    Clean-up code in app.js and create console.log for html generation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -11,29 +12,17 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const writeFileAsync = util.promisify(fs.writeFile);
-//const { fetchAsyncQuestionPropertyQuestionProperty } = require("inquirer/lib/utils/utils");
-//const { ifStatement } = require("@babel/types");
 
 const employees = [];
-//let createdManagers = [];
-//let createdInterns = [];
-//let createdEngineers = [];
-//function to generate employee
-//ask what employee wants to enter
-//function for additonal qs(manager, intern, engineer)
 
-
-//function to generate manager
-//ask what type of employee want to create
-//function to handle intern and engineer
-//building html rendering function file build words
-//inquirer.prompt().then;
+//GENERATE TEAM.html
 
 function generateMyTeam() {
     const teamHTML = render(employees);
     writeFileAsync(outputPath, teamHTML);
+    console.log("See your team output/team.html!")
 }
-//RUN APPLICATION
+//FUNCTION TO BEGIN APPLICATION
 
 function run() {
     createEmployee();
@@ -83,9 +72,6 @@ function nextEmployee() {
             }
             if (answers.next == 'NO') {
                 console.log(employees);
-                //render
-                //console.log(createdEngineers);
-                //console.log(createdInterns);
             }
 
         })
@@ -190,31 +176,6 @@ function createManager() {
         });
 };
 
-//module.exports = employees
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+//RUN APP
 
 run();
